62e9d1d3b288620ca1ff8a29757101222b81be36
mrlectus <lilbrown3000@gmail.com>
2025-04-12 22:07:26 +0100

    removed previous logic added streaming instead

--- providers/async.tsx
+++ providers/async.tsx
@@ -1,16 +1,41 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+  isServer,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
 
 export const AsyncProvider = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = React.useState(() => new QueryClient());
+  const queryClient = getQueryClient();
+
   return (
-    <React.Fragment>
-      <QueryClientProvider client={queryClient}>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </React.Fragment>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
